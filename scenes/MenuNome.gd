2a131caf0775983763cc4463f3f4d94e064c0e23
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2026-06-21 17:52:13 -0300

    Add files via upload

---
+++ scenes/MenuNome.gd
@@ -0,0 +1,5 @@
+extends TextureRect
+
+func _on_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Fase2/Exercicio2/conserte_a_rua_tscn.tscn")
+	# Colocar caminho da fase 1 exercício 1!!!!!
