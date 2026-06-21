9685b87880243be5e45744691a6bb7501a0b8266
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2026-06-21 18:03:17 -0300

    Add files via upload

---
+++ Fase2/Exercicio2/botao_voltar.gd
@@ -0,0 +1,9 @@
+extends Control
+
+func _ready() -> void:
+	# Conecta o sinal de clique ao método
+	self.pressed.connect(_on_button_pressed)
+
+func _on_button_pressed() -> void:
+	# Troca para a cena do menu principal
+	get_tree().change_scene_to_file("res://scenes/menu_principal.tscn")
