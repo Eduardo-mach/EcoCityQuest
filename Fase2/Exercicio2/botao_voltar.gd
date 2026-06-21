7cdf30a35b89eccde1f6b0b9327ce567e44ce6bb
Rhyan Wilezelek <rhyanwilezelek@gmail.com>
2026-06-21 17:46:09 -0300

    Delete Fase2 directory

--- Fase2/Exercicio2/botao_voltar.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-func _ready() -> void:
-	# Conecta o sinal de clique ao método
-	self.pressed.connect(_on_button_pressed)
-
-func _on_button_pressed() -> void:
-	# Troca para a cena do menu principal
-	get_tree().change_scene_to_file("res://scenes/menu_principal.tscn")
